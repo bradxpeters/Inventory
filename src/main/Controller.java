368d50f6492daddfc696d6a589e9ffe619ac881a
Brad Peters <brad@Brads-MacBook-Pro.local>
2021-01-21 10:50:30 -0600

    Refactored getters and setters in main controller

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -7,6 +7,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import parts.InHouse;
@@ -16,17 +18,20 @@ import parts.PartsController;
 import products.Product;
 import products.ProductsController;
 
+import java.awt.*;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.ResourceBundle;
 
 /**
- * The type Controller.
+ * The Main Controller.
  */
 public class Controller implements Initializable {
 
@@ -57,17 +62,11 @@ public class Controller implements Initializable {
     @FXML
     private TextField searchProductField;
 
-    /**
-     * The Parts table.
-     */
     @FXML
-    TableView<Part> partsTable;
+    private TableView<Part> partsTable;
 
-    /**
-     * The Products table.
-     */
     @FXML
-    TableView<Product> productsTable;
+    private TableView<Product> productsTable;
 
     @FXML
     private TableColumn<Part,Integer> partId;
@@ -93,13 +92,24 @@ public class Controller implements Initializable {
     @FXML
     private TableColumn<Product,Double> productPrice;
 
+    @FXML
+    private Hyperlink documentationLink;
+
     /**
      * Handle exit button.
      */
     @FXML
-    void handleExitButton() {
+    private void handleExitButton() {
         var stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
+    }
+
+    /**
+     * Handle clicking documentation link.
+     */
+    @FXML
+    private void handleClickDocumentationLink() throws IOException, URISyntaxException {
+        Desktop.getDesktop().browse(Paths.get("javadocs/index.html").toUri());
     }
 
     /**
@@ -108,7 +118,7 @@ public class Controller implements Initializable {
      * @throws IOException the io exception
      */
     @FXML
-    void handleAddPartButton() throws IOException {
+    private void handleAddPartButton() throws IOException {
         var fxmlLoader = new FXMLLoader(Part.class.getResource("addPartForm.fxml"));
         var root = (Parent) fxmlLoader.load();
         var stage = new Stage();
@@ -120,7 +130,7 @@ public class Controller implements Initializable {
      * Handle delete part button.
      */
     @FXML
-    void handleDeletePartButton() {
+    private void handleDeletePartButton() {
        Inventory.getInstance().deletePart(partsTable.getSelectionModel().getSelectedItem());
     }
 
@@ -128,7 +138,7 @@ public class Controller implements Initializable {
      * Handle delete product button.
      */
     @FXML
-    void handleDeleteProductButton() {
+    private void handleDeleteProductButton() {
         Inventory.getInstance().deleteProduct(productsTable.getSelectionModel().getSelectedItem());
     }
 
@@ -187,7 +197,7 @@ public class Controller implements Initializable {
      */
     public void handlePartSelectionChange(Part newValue) {
         if (newValue != null) {
-            modifyPartButton.setDisable(false);
+            this.getModifyPartButton().setDisable(false);
         }
     }
 
@@ -198,29 +208,29 @@ public class Controller implements Initializable {
      */
     public void handleProductSelectionChange(Product newValue) {
         if (newValue != null) {
-            modifyProductButton.setDisable(false);
+            this.getModifyProductButton().setDisable(false);
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // Initialize cell factories
-        partId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        partName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        partStock.setCellValueFactory(new PropertyValueFactory<>("stock"));
-        partPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        this.getPartId().setCellValueFactory(new PropertyValueFactory<>("id"));
+        this.getPartName().setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.getPartStock().setCellValueFactory(new PropertyValueFactory<>("stock"));
+        this.getPartPrice().setCellValueFactory(new PropertyValueFactory<>("price"));
 
-        productId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        productName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        productStock.setCellValueFactory(new PropertyValueFactory<>("stock"));
-        productPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        this.getProductId().setCellValueFactory(new PropertyValueFactory<>("id"));
+        this.getProductName().setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.getProductStock().setCellValueFactory(new PropertyValueFactory<>("stock"));
+        this.getProductPrice().setCellValueFactory(new PropertyValueFactory<>("price"));
 
         // Link table to observable list singleton
-        partsTable.setItems(Inventory.getInstance().getAllParts());
-        productsTable.setItems(Inventory.getInstance().getAllProducts());
+        this.getPartsTable().setItems(Inventory.getInstance().getAllParts());
+        this.getProductsTable().setItems(Inventory.getInstance().getAllProducts());
 
         // Listen to search fields
-        searchPartField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+        this.getSearchPartField().textProperty().addListener((observableValue, oldValue, newValue) -> {
             int partId = -1;
             try {
                 partId = Integer.parseInt(newValue);
@@ -229,19 +239,19 @@ public class Controller implements Initializable {
             if (partId != -1) {
                 var tempList = FXCollections.observableList(new ArrayList<Part>());
                 tempList.add(Inventory.getInstance().lookupPart(partId));
-                partsTable.setItems(tempList);
+                this.getPartsTable().setItems(tempList);
             } else {
-                partsTable.setItems(Inventory.getInstance().lookupPart(newValue));
+                this.getPartsTable().setItems(Inventory.getInstance().lookupPart(newValue));
             }
 
-            if (partsTable.getItems().size() == 0) {
+            if (this.getPartsTable().getItems().size() == 0) {
                 var alert = new Alert(Alert.AlertType.ERROR, "No result found!");
                 alert.setHeaderText("Search error");
                 alert.showAndWait();
             }
         });
 
-        searchProductField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+        this.getSearchProductField().textProperty().addListener((observableValue, oldValue, newValue) -> {
             int productId = -1;
             try {
                 productId = Integer.parseInt(newValue);
@@ -250,12 +260,12 @@ public class Controller implements Initializable {
             if (productId != -1) {
                 var tempList = FXCollections.observableList(new ArrayList<Product>());
                 tempList.add(Inventory.getInstance().lookupProduct(productId));
-                productsTable.setItems(tempList);
+                this.getProductsTable().setItems(tempList);
             } else {
-                productsTable.setItems(Inventory.getInstance().lookupProduct(newValue));
+                this.getProductsTable().setItems(Inventory.getInstance().lookupProduct(newValue));
             }
 
-            if (productsTable.getItems().size() == 0) {
+            if (this.getProductsTable().getItems().size() == 0) {
                 var alert = new Alert(Alert.AlertType.ERROR, "No result found!");
                 alert.setHeaderText("Search error");
                 alert.showAndWait();
@@ -264,14 +274,16 @@ public class Controller implements Initializable {
 
 
         // Modify buttons should be disabled until a selection is made
-        modifyPartButton.setDisable(true);
-        modifyProductButton.setDisable(true);
-        partsTable.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
-            handlePartSelectionChange(newValue);
-        });
-        productsTable.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
-            handleProductSelectionChange(newValue);
-        });
+        this.getModifyPartButton().setDisable(true);
+        this.getModifyProductButton().setDisable(true);
+        this.getPartsTable()
+            .getSelectionModel()
+            .selectedItemProperty()
+            .addListener((observableValue, oldValue, newValue) -> { handlePartSelectionChange(newValue); });
+        this.getProductsTable()
+            .getSelectionModel()
+            .selectedItemProperty()
+            .addListener((observableValue, oldValue, newValue) -> { handleProductSelectionChange(newValue); });
 
         // Create some starter parts and products
         var inHousePartNames = Arrays.asList("brake pad", "rim");
@@ -340,5 +352,365 @@ public class Controller implements Initializable {
                     Inventory.getInstance().addProduct(product);
                 }
         );
+    }
+
+    /**
+     * Gets add part button.
+     *
+     * @return the add part button
+     */
+    public Button getAddPartButton() {
+        return addPartButton;
+    }
+
+    /**
+     * Sets add part button.
+     *
+     * @param addPartButton the add part button
+     */
+    public void setAddPartButton(Button addPartButton) {
+        this.addPartButton = addPartButton;
+    }
+
+    /**
+     * Gets modify part button.
+     *
+     * @return the modify part button
+     */
+    public Button getModifyPartButton() {
+        return modifyPartButton;
+    }
+
+    /**
+     * Sets modify part button.
+     *
+     * @param modifyPartButton the modify part button
+     */
+    public void setModifyPartButton(Button modifyPartButton) {
+        this.modifyPartButton = modifyPartButton;
+    }
+
+    /**
+     * Gets delete part button.
+     *
+     * @return the delete part button
+     */
+    public Button getDeletePartButton() {
+        return deletePartButton;
+    }
+
+    /**
+     * Sets delete part button.
+     *
+     * @param deletePartButton the delete part button
+     */
+    public void setDeletePartButton(Button deletePartButton) {
+        this.deletePartButton = deletePartButton;
+    }
+
+    /**
+     * Gets add product button.
+     *
+     * @return the add product button
+     */
+    public Button getAddProductButton() {
+        return addProductButton;
+    }
+
+    /**
+     * Sets add product button.
+     *
+     * @param addProductButton the add product button
+     */
+    public void setAddProductButton(Button addProductButton) {
+        this.addProductButton = addProductButton;
+    }
+
+    /**
+     * Gets modify product button.
+     *
+     * @return the modify product button
+     */
+    public Button getModifyProductButton() {
+        return modifyProductButton;
+    }
+
+    /**
+     * Sets modify product button.
+     *
+     * @param modifyProductButton the modify product button
+     */
+    public void setModifyProductButton(Button modifyProductButton) {
+        this.modifyProductButton = modifyProductButton;
+    }
+
+    /**
+     * Gets delete product button.
+     *
+     * @return the delete product button
+     */
+    public Button getDeleteProductButton() {
+        return deleteProductButton;
+    }
+
+    /**
+     * Sets delete product button.
+     *
+     * @param deleteProductButton the delete product button
+     */
+    public void setDeleteProductButton(Button deleteProductButton) {
+        this.deleteProductButton = deleteProductButton;
+    }
+
+    /**
+     * Gets exit button.
+     *
+     * @return the exit button
+     */
+    public Button getExitButton() {
+        return exitButton;
+    }
+
+    /**
+     * Sets exit button.
+     *
+     * @param exitButton the exit button
+     */
+    public void setExitButton(Button exitButton) {
+        this.exitButton = exitButton;
+    }
+
+    /**
+     * Gets search part field.
+     *
+     * @return the search part field
+     */
+    public TextField getSearchPartField() {
+        return searchPartField;
+    }
+
+    /**
+     * Sets search part field.
+     *
+     * @param searchPartField the search part field
+     */
+    public void setSearchPartField(TextField searchPartField) {
+        this.searchPartField = searchPartField;
+    }
+
+    /**
+     * Gets search product field.
+     *
+     * @return the search product field
+     */
+    public TextField getSearchProductField() {
+        return searchProductField;
+    }
+
+    /**
+     * Sets search product field.
+     *
+     * @param searchProductField the search product field
+     */
+    public void setSearchProductField(TextField searchProductField) {
+        this.searchProductField = searchProductField;
+    }
+
+    /**
+     * Gets parts table.
+     *
+     * @return the parts table
+     */
+    public TableView<Part> getPartsTable() {
+        return partsTable;
+    }
+
+    /**
+     * Sets parts table.
+     *
+     * @param partsTable the parts table
+     */
+    public void setPartsTable(TableView<Part> partsTable) {
+        this.partsTable = partsTable;
+    }
+
+    /**
+     * Gets products table.
+     *
+     * @return the products table
+     */
+    public TableView<Product> getProductsTable() {
+        return productsTable;
+    }
+
+    /**
+     * Sets products table.
+     *
+     * @param productsTable the products table
+     */
+    public void setProductsTable(TableView<Product> productsTable) {
+        this.productsTable = productsTable;
+    }
+
+    /**
+     * Gets part id.
+     *
+     * @return the part id
+     */
+    public TableColumn<Part, Integer> getPartId() {
+        return partId;
+    }
+
+    /**
+     * Sets part id.
+     *
+     * @param partId the part id
+     */
+    public void setPartId(TableColumn<Part, Integer> partId) {
+        this.partId = partId;
+    }
+
+    /**
+     * Gets part name.
+     *
+     * @return the part name
+     */
+    public TableColumn<Part, String> getPartName() {
+        return partName;
+    }
+
+    /**
+     * Sets part name.
+     *
+     * @param partName the part name
+     */
+    public void setPartName(TableColumn<Part, String> partName) {
+        this.partName = partName;
+    }
+
+    /**
+     * Gets part stock.
+     *
+     * @return the part stock
+     */
+    public TableColumn<Part, Integer> getPartStock() {
+        return partStock;
+    }
+
+    /**
+     * Sets part stock.
+     *
+     * @param partStock the part stock
+     */
+    public void setPartStock(TableColumn<Part, Integer> partStock) {
+        this.partStock = partStock;
+    }
+
+    /**
+     * Gets part price.
+     *
+     * @return the part price
+     */
+    public TableColumn<Part, Double> getPartPrice() {
+        return partPrice;
+    }
+
+    /**
+     * Sets part price.
+     *
+     * @param partPrice the part price
+     */
+    public void setPartPrice(TableColumn<Part, Double> partPrice) {
+        this.partPrice = partPrice;
+    }
+
+    /**
+     * Gets product id.
+     *
+     * @return the product id
+     */
+    public TableColumn<Product, Integer> getProductId() {
+        return productId;
+    }
+
+    /**
+     * Sets product id.
+     *
+     * @param productId the product id
+     */
+    public void setProductId(TableColumn<Product, Integer> productId) {
+        this.productId = productId;
+    }
+
+    /**
+     * Gets product name.
+     *
+     * @return the product name
+     */
+    public TableColumn<Product, String> getProductName() {
+        return productName;
+    }
+
+    /**
+     * Sets product name.
+     *
+     * @param productName the product name
+     */
+    public void setProductName(TableColumn<Product, String> productName) {
+        this.productName = productName;
+    }
+
+    /**
+     * Gets product stock.
+     *
+     * @return the product stock
+     */
+    public TableColumn<Product, Integer> getProductStock() {
+        return productStock;
+    }
+
+    /**
+     * Sets product stock.
+     *
+     * @param productStock the product stock
+     */
+    public void setProductStock(TableColumn<Product, Integer> productStock) {
+        this.productStock = productStock;
+    }
+
+    /**
+     * Gets product price.
+     *
+     * @return the product price
+     */
+    public TableColumn<Product, Double> getProductPrice() {
+        return productPrice;
+    }
+
+    /**
+     * Sets product price.
+     *
+     * @param productPrice the product price
+     */
+    public void setProductPrice(TableColumn<Product, Double> productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    /**
+     * Gets documentation link.
+     *
+     * @return the documentation link
+     */
+    public Hyperlink getDocumentationLink() {
+        return documentationLink;
+    }
+
+    /**
+     * Sets documentation link.
+     *
+     * @param documentationLink the documentation link
+     */
+    public void setDocumentationLink(Hyperlink documentationLink) {
+        this.documentationLink = documentationLink;
     }
 }
